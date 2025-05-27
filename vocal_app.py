fb098b40f5d56f82d2e62bbf15f01f7e3f14185f
Iago Virgílio <iagovirgilio@gmail.com>
2025-05-27 11:42:56 -0300

    Criar iagem docker

--- vocal_app.py
+++ vocal_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import sys
 from io import StringIO
+import os
+
+port = int(os.environ.get("PORT", 8501))
+
 
 # Importar a classe VocalRangeCalculator
 class VocalRangeCalculator:
@@ -588,4 +592,10 @@ def main():
         """)
 
 if __name__ == "__main__":
-    main()
+    import sys
+    if len(sys.argv) == 1:
+        import streamlit.web.cli as stcli
+        sys.argv = ["streamlit", "run", "vocal_app.py", "--server.port", str(port), "--server.address", "0.0.0.0"]
+        sys.exit(stcli.main())
+    else:
+        main()
